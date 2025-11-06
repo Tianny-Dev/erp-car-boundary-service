ecdcaa431da4fbf9e4d3850edb25abbaece7939e
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2025-11-06 12:25:23 +0800

    feat(routes): register finance pages

---
+++ routes/finance.php
@@ -0,0 +1,15 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware(['auth', 'verified'])->prefix('finance')->name('finance.')->group(function () {
+    Route::get('/dashboard', fn() => Inertia::render('finance/dashboard/Index'))->name('dashboard');
+    Route::get('/boundary-contracts', fn() => Inertia::render('finance/boundary-contracts/Index'))->name('boundaryContracts');
+    Route::get('/revenue-management', fn() => Inertia::render('finance/revenue-management/Index'))->name('revenueManagement');
+    Route::get('/expense-management', fn() => Inertia::render('finance/expense-management/Index'))->name('expenseManagement');
+    Route::get('/reports-and-analytics', fn() => Inertia::render('finance/reports-and-analytics/Index'))->name('reportsAndAnalytics');
+    Route::get('/support-center', fn() => Inertia::render('finance/support-center/Index'))->name('supportCenter');
+    Route::get('/notifications', fn() => Inertia::render('finance/notifications/Index'))->name('notifications');
+});
