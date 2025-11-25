3c0b53e8904f0a18a334670e0dd02992699fb216
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2025-11-25 15:18:34 +0800

    feat(ui):added filter for status, payment option, and time period for revenue and expense management of manager

--- resources/js/pages/owner/expense-management/Index.vue
+++ resources/js/pages/owner/expense-management/Index.vue
@@ -316,8 +316,8 @@ watch(
       <div class="rounded-lg border">
         <Table>
           <TableHeader>
-            <TableRow>
-              <template v-if="!isGrouped">
+            <template v-if="!isGrouped">
+              <TableRow>
                 <TableHead>Invoice No</TableHead>
                 <TableHead>Expense Type</TableHead>
                 <TableHead>Amount</TableHead>
@@ -327,27 +327,27 @@ watch(
                 <TableHead>Branch</TableHead>
                 <TableHead>Payment Option</TableHead>
                 <TableHead>Action</TableHead>
-              </template>
+              </TableRow>
+            </template>
 
-              <template v-else>
-                <TableRow>
-                  <TableHead>
-                    {{
-                      filters.timePeriod === 'daily'
-                        ? 'Date'
-                        : filters.timePeriod === 'weekly'
-                          ? 'Week'
-                          : filters.timePeriod === 'monthly'
-                            ? 'Month'
-                            : filters.timePeriod === 'yearly'
-                              ? 'Year'
-                              : 'Group'
-                    }}
-                  </TableHead>
-                  <TableHead>Total Expense</TableHead>
-                </TableRow>
-              </template>
-            </TableRow>
+            <template v-else>
+              <TableRow>
+                <TableHead>
+                  {{
+                    filters.timePeriod === 'daily'
+                      ? 'Date'
+                      : filters.timePeriod === 'weekly'
+                        ? 'Week'
+                        : filters.timePeriod === 'monthly'
+                          ? 'Month'
+                          : filters.timePeriod === 'yearly'
+                            ? 'Year'
+                            : 'Group'
+                  }}
+                </TableHead>
+                <TableHead>Total Expense</TableHead>
+              </TableRow>
+            </template>
           </TableHeader>
 
           <TableBody>
