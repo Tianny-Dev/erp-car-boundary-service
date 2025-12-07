821fadf367008c72bd372cc4944923e9c5935229
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2025-12-07 11:21:06 +0800

    feat(routes): added managemenr and application of driver routes for manager

--- routes/manager.php
+++ routes/manager.php
@@ -1,22 +1,21 @@
 <?php
 
 use App\Http\Controllers\Manager\BoundaryContractController;
-use App\Http\Controllers\Manager\BranchDriverController;
 use App\Http\Controllers\Manager\DashboardController;
+use App\Http\Controllers\Manager\DriverApplicationController;
+use App\Http\Controllers\Manager\DriverManagementController;
 use App\Http\Controllers\Manager\RevenueManagementController;
 use App\Http\Controllers\Manager\ExpenseManagementController;
 use App\Http\Controllers\Manager\ReportAndAnalyticController;
 use App\Http\Controllers\Manager\SupportCenterController;
 use App\Http\Controllers\Manager\NotificationController;
 use App\Http\Controllers\Manager\PayOutController;
-use App\Http\Controllers\Manager\SuspendDriverController;
 use App\Http\Controllers\Manager\VehicleController;
 use App\Http\Controllers\Manager\VehicleDriverController;
 
 use App\Http\Controllers\Manager\DetailsDriverController;
 use App\Http\Controllers\Manager\ReportDriverController;
 
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'user_type:manager'])->prefix('manager')->name('manager.')->group(function () {
@@ -29,18 +28,16 @@ Route::middleware(['auth', 'verified', 'user_type:manager'])->prefix('manager')-
     Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications');
     Route::get('/payout', [PayOutController::class, 'index'])->name('payout');
 
-    Route::resource('drivers', BranchDriverController::class);
+    Route::resource('drivers', DriverManagementController::class);
+    Route::resource('drivers-application', DriverApplicationController::class);
     Route::resource('vehicles', VehicleController::class);
     Route::resource('vehicle-drivers', VehicleDriverController::class);
 
-    Route::put('/drivers/{id}/status', [BranchDriverController::class, 'updateStatus'])
+    Route::put('/drivers/{id}/status', [DriverManagementController::class, 'updateStatus'])
     ->name('drivers.updateStatus');
 
     Route::get('/driverreport', [ReportDriverController::class, 'index'])->name('driverownerreport');
     Route::get('/driverreport/export', [ReportDriverController::class, 'export'])->name('driverownerreport.export');
     Route::get('driverreport/details', [DetailsDriverController::class, 'show'])->name('driverownerreport.details');
     Route::get('/driverreport/details/export', [DetailsDriverController::class, 'exportDetails'])->name('driverownerreport_details.export');
-
-    // Suspend Driver
-    Route::resource('suspend-drivers', SuspendDriverController::class);
 });
