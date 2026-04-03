027e9ec4b41b19a6a3ce931b83109a309d25ec68
joshi <jespadilla422@gmail.com>
2026-04-03 21:04:51 +0800

    feat(boundary-contract): add cronjob for expired statuses

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\ExpireBoundaryContracts;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(ExpireBoundaryContracts::class)
+    ->dailyAt('00:05') // Runs 5 mins past midnight daily
+    ->withoutOverlapping()
+    ->runInBackground()
+    ->onFailure(function () {
+        Log::error('ExpireBoundaryContracts cron job failed.');
+    });
